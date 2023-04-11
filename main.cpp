aa9a0cc22fe7df0a58bc71c496e2d53dbdd65e59
Aryan Dua <cs5200475@iitd.ac.in>
2023-04-11 23:38:02 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
+
+using namespace std;
 
 void sort(char* buffer, int bufferSize){
-//implement quicksort
+//implement sort
+    return;
 }
 
-int external_merge_sort_withstop ( const char* input , const char* output ,
-const long key_count , const int k = 2 , const int num_merges = 0 ){
+int external_merge_sort_withstop ( const char* input , const char* output , const long key_count , const int k = 2 , const int num_merges = 0 ){
 
     FILE* ptr;
     ptr = fopen(input, "r");
-
 
     long bufferSize = 1024 * 1024; //10^6
     char* buffer = new char[bufferSize];
@@ -33,15 +34,19 @@ const long key_count , const int k = 2 , const int num_merges = 0 ){
         sort(buffer, bufferSize);
         //buffer has the sorted values
         FILE* ptr_new;
-        ptr_new = fopen('temp.' + atoi(stage) + '.' + atoi(num_runs));
+        string s = "temp." + to_string(stage) + "." + to_string(num_runs);
+        char* char_array = new char[s.length() + 1];
+        strcpy(char_array, s.c_str());
+        ptr_new = fopen(char_array, "r");
         fputs(buffer, ptr_new);        
         num_runs++;
     }
 
     fclose(ptr);
+    return 0;
 }
 
 
 int main(){
-
+    return 0;
 }
